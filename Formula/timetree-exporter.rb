76b9c66f7f0fccbb26184f2d5299d41e2207500f
蔡鳳駿 <eoleedimin@gmail.com>
2024-12-24 16:18:25 +0800

    style: format the formula

--- Formula/timetree-exporter.rb
+++ Formula/timetree-exporter.rb
@@ -1,66 +1,66 @@
 class TimetreeExporter < Formula
-    include Language::Python::Virtualenv
+  include Language::Python::Virtualenv
 
-    desc "Tool for exporting TimeTree Calendar and converting to iCal format (.ics)"
-    homepage "https://github.com/eoleedi/TimeTree-Exporter"
-    url "https://files.pythonhosted.org/packages/3b/da/03876c409c98ef6a8e32b6f04370adc473ef9b1992adf139d5d1248c5588/timetree_exporter-0.4.1.tar.gz"
-    version "0.4.1"
-    sha256 "638d88bd1ccfe3c59c209eca9a9eb16ad9175bbaa6e6a212e05e43ca1dcb3879"
-    license "MIT"
+  desc 'Tool for exporting TimeTree Calendar and converting to iCal format (.ics)'
+  homepage 'https://github.com/eoleedi/TimeTree-Exporter'
+  url 'https://files.pythonhosted.org/packages/3b/da/03876c409c98ef6a8e32b6f04370adc473ef9b1992adf139d5d1248c5588/timetree_exporter-0.4.1.tar.gz'
+  version '0.4.1'
+  sha256 '638d88bd1ccfe3c59c209eca9a9eb16ad9175bbaa6e6a212e05e43ca1dcb3879'
+  license 'MIT'
 
-    depends_on "python@3.13"
+  depends_on 'python@3.13'
 
-    resource "certifi" do
-        url "https://files.pythonhosted.org/packages/0f/bd/1d41ee578ce09523c81a15426705dd20969f5abf006d1afe8aeff0dd776a/certifi-2024.12.14.tar.gz"
-        sha256 "b650d30f370c2b724812bee08008be0c4163b163ddaec3f2546c1caf65f191db"
-    end
+  resource 'certifi' do
+    url 'https://files.pythonhosted.org/packages/0f/bd/1d41ee578ce09523c81a15426705dd20969f5abf006d1afe8aeff0dd776a/certifi-2024.12.14.tar.gz'
+    sha256 'b650d30f370c2b724812bee08008be0c4163b163ddaec3f2546c1caf65f191db'
+  end
 
-    resource "charset-normalizer" do
-        url "https://files.pythonhosted.org/packages/f2/4f/e1808dc01273379acc506d18f1504eb2d299bd4131743b9fc54d7be4df1e/charset_normalizer-3.4.0.tar.gz"
-        sha256 "223217c3d4f82c3ac5e29032b3f1c2eb0fb591b72161f86d93f5719079dae93e"
-    end
+  resource 'charset-normalizer' do
+    url 'https://files.pythonhosted.org/packages/f2/4f/e1808dc01273379acc506d18f1504eb2d299bd4131743b9fc54d7be4df1e/charset_normalizer-3.4.0.tar.gz'
+    sha256 '223217c3d4f82c3ac5e29032b3f1c2eb0fb591b72161f86d93f5719079dae93e'
+  end
 
-    resource "icalendar" do
-        url "https://files.pythonhosted.org/packages/03/44/c9fdf8f6442e5bef2635f25dbb3d3ac12d5f2ecf5223bd94acaba51ceda4/icalendar-6.1.0.tar.gz"
-        sha256 "43c2db8632959d634f4e48f6e6131e706bf2cdddad488cf0b72fda079b796bad"
-    end
+  resource 'icalendar' do
+    url 'https://files.pythonhosted.org/packages/03/44/c9fdf8f6442e5bef2635f25dbb3d3ac12d5f2ecf5223bd94acaba51ceda4/icalendar-6.1.0.tar.gz'
+    sha256 '43c2db8632959d634f4e48f6e6131e706bf2cdddad488cf0b72fda079b796bad'
+  end
 
-    resource "idna" do
-        url "https://files.pythonhosted.org/packages/f1/70/7703c29685631f5a7590aa73f1f1d3fa9a380e654b86af429e0934a32f7d/idna-3.10.tar.gz"
-        sha256 "12f65c9b470abda6dc35cf8e63cc574b1c52b11df2c86030af0ac09b01b13ea9"
-    end
+  resource 'idna' do
+    url 'https://files.pythonhosted.org/packages/f1/70/7703c29685631f5a7590aa73f1f1d3fa9a380e654b86af429e0934a32f7d/idna-3.10.tar.gz'
+    sha256 '12f65c9b470abda6dc35cf8e63cc574b1c52b11df2c86030af0ac09b01b13ea9'
+  end
 
-    resource "python-dateutil" do
-        url "https://files.pythonhosted.org/packages/66/c0/0c8b6ad9f17a802ee498c46e004a0eb49bc148f2fd230864601a86dcf6db/python-dateutil-2.9.0.post0.tar.gz"
-        sha256 "37dd54208da7e1cd875388217d5e00ebd4179249f90fb72437e91a35459a0ad3"
-    end
+  resource 'python-dateutil' do
+    url 'https://files.pythonhosted.org/packages/66/c0/0c8b6ad9f17a802ee498c46e004a0eb49bc148f2fd230864601a86dcf6db/python-dateutil-2.9.0.post0.tar.gz'
+    sha256 '37dd54208da7e1cd875388217d5e00ebd4179249f90fb72437e91a35459a0ad3'
+  end
 
-    resource "requests" do
-        url "https://files.pythonhosted.org/packages/63/70/2bf7780ad2d390a8d301ad0b550f1581eadbd9a20f896afe06353c2a2913/requests-2.32.3.tar.gz"
-        sha256 "55365417734eb18255590a9ff9eb97e9e1da868d4ccd6402399eaf68af20a760"
-    end
+  resource 'requests' do
+    url 'https://files.pythonhosted.org/packages/63/70/2bf7780ad2d390a8d301ad0b550f1581eadbd9a20f896afe06353c2a2913/requests-2.32.3.tar.gz'
+    sha256 '55365417734eb18255590a9ff9eb97e9e1da868d4ccd6402399eaf68af20a760'
+  end
 
-    resource "six" do
-        url "https://files.pythonhosted.org/packages/94/e7/b2c673351809dca68a0e064b6af791aa332cf192da575fd474ed7d6f16a2/six-1.17.0.tar.gz"
-        sha256 "ff70335d468e7eb6ec65b95b99d3a2836546063f63acc5171de367e834932a81"
-    end
+  resource 'six' do
+    url 'https://files.pythonhosted.org/packages/94/e7/b2c673351809dca68a0e064b6af791aa332cf192da575fd474ed7d6f16a2/six-1.17.0.tar.gz'
+    sha256 'ff70335d468e7eb6ec65b95b99d3a2836546063f63acc5171de367e834932a81'
+  end
 
-    resource "tzdata" do
-        url "https://files.pythonhosted.org/packages/e1/34/943888654477a574a86a98e9896bae89c7aa15078ec29f490fef2f1e5384/tzdata-2024.2.tar.gz"
-        sha256 "7d85cc416e9382e69095b7bdf4afd9e3880418a2413feec7069d533d6b4e31cc"
-    end
+  resource 'tzdata' do
+    url 'https://files.pythonhosted.org/packages/e1/34/943888654477a574a86a98e9896bae89c7aa15078ec29f490fef2f1e5384/tzdata-2024.2.tar.gz'
+    sha256 '7d85cc416e9382e69095b7bdf4afd9e3880418a2413feec7069d533d6b4e31cc'
+  end
 
-    resource "urllib3" do
-        url "https://files.pythonhosted.org/packages/aa/63/e53da845320b757bf29ef6a9062f5c669fe997973f966045cb019c3f4b66/urllib3-2.3.0.tar.gz"
-        sha256 "f8c5449b3cf0861679ce7e0503c7b44b5ec981bec0d1d3795a07f1ba96f0204d"
-    end
+  resource 'urllib3' do
+    url 'https://files.pythonhosted.org/packages/aa/63/e53da845320b757bf29ef6a9062f5c669fe997973f966045cb019c3f4b66/urllib3-2.3.0.tar.gz'
+    sha256 'f8c5449b3cf0861679ce7e0503c7b44b5ec981bec0d1d3795a07f1ba96f0204d'
+  end
 
-    def install
-        virtualenv_create(libexec, "python3")
-        virtualenv_install_with_resources
-    end
+  def install
+    virtualenv_create(libexec, 'python3')
+    virtualenv_install_with_resources
+  end
 
-    test do
-        assert_match "Usage:", shell_output("#{bin}/timetree-exporter --help")
-    end
+  test do
+    assert_match 'Usage:', shell_output("#{bin}/timetree-exporter --help")
+  end
 end
